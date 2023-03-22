c45c7ebd417bce70ff71f611a205e5251f90f516
David Chen <qydchen@gmail.com>
2023-03-21 18:03:05 -0700

    session 5

--- batch_37/session_5.js
+++ batch_37/session_5.js
@@ -62,3 +62,77 @@ class MinHeap {
     }
   };
 }
+
+
+class Trie {
+  #root = {};
+
+  insert(word) {
+    // O(n) space & time where n is length of word
+    if (word.length === 0) {
+      return;
+    }
+
+    let currentNode = this.#root;
+    for (let char of word) {
+      if (!(char in currentNode)) {
+        currentNode[char] = {};
+      }
+      currentNode = currentNode[char];
+    }
+    currentNode["*"] = {};
+  }
+
+  search(word) {
+    // O(n) time | O(1) space where n is length of word
+    if (word.length === 0) {
+      return false;
+    }
+    let currentNode = this.#root;
+    for (let char of word) {
+      if (!(char in currentNode)) {
+        return false;
+      }
+      currentNode = currentNode[char];
+    }
+    return "*" in currentNode;
+  }
+
+  startsWith(prefix) {
+    // O(n) time | O(1) space where n is length of word
+    if (prefix.length === 0) {
+      return true;
+    }
+    let currentNode = this.#root;
+    for (let char of prefix) {
+      if (!(char in currentNode)) {
+        return false;
+      }
+      currentNode = currentNode[char];
+    }
+    return true;
+  }
+
+  get root() {
+    return this.#root;
+  }
+}
+
+
+// Homework
+
+// Heap problems
+// - Top K Frequent Words. (https://leetcode.com/problems/top-k-frequent-words/)
+// - K Closest Points to Origin. (https://leetcode.com/problems/k-closest-points-to-origin)
+// - Write an algorithm `isMinHeap(array)` that checks if a given array represents a MinHeap.
+// - Implement a `MedianHeap` class to keep track of the median of a given set of integers. The API should have the following three functions:
+//     - `insert(int)` in `O(log n)`
+//     - `peekMedian()` in `O(1)`
+//     - `deleteMedian()` in `O(log n)`
+// - Implement Heapsort. (https://en.wikipedia.org/wiki/Heapsort)
+
+// Trie Problems
+// - Simple Regex Searcher. (https://leetcode.com/problems/design-add-and-search-words-data-structure/)
+// - Word Search. (https://leetcode.com/problems/word-search-ii/)
+// - Magic Dictionary. (https://leetcode.com/problems/implement-magic-dictionary/)
+// - Simple Word Break. (https://leetcode.com/problems/word-break/)
